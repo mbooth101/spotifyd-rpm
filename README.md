fb91f87f085f0739b100cbd2a9258ae9ae171e05
Mat Booth <mat.booth@gmail.com>
2025-10-21 23:35:01 +0100

    Documentation enhancements

--- README.md
+++ README.md
@@ -1,14 +1,49 @@
 # Spotifyd RPMs
 
-This repository contains specfiles for creating an RPM-installable version of [Spotifyd](https://github.com/Spotifyd/spotifyd).
+An RPM packaged version of spotifyd, an open source Spotify client running as a UNIX daemon.
 
-The binary RPMs are hosted in a [COPR repository](https://copr.fedorainfracloud.org/coprs/mbooth/spotifyd/).
+By default spotifyd behaves purely as a "Spotify Connect" device for official Spotify clients on the same network as the machine running the daemon.
 
-### Maintenance
+## Installation Instructions
 
-Use the `rust2rpm` tool to create/update rust packages:
+As root:
 
+1. Install spotifyd:
+
+```text
+# dnf copr enable mbooth/spotifyd
+# dnf install spotifyd
 ```
-$ sudo dnf install rust2rpm
-$ rust2rpm --store-crate --no-rpmautospec --ignore-missing-license-files <crate_name>@<version>
+
+2. Enable the spotifyd service:
+
+```text
+# systemctl enable --now spotifyd.service
 ```
+
+3. Open the Multicast DNS and Spotify Connect ports:
+
+```text
+# firewall-cmd --permanent --add-service=mdns
+# firewall-cmd --permanent --add-service=spotify-connect
+# systemctl reload firewalld
+```
+
+4. From an official Spotify client, spotifyd will be seen in the list of devices to which you may connect:
+
+![Official Spotify Client Device Choice Menu](https://mbooth.fedorapeople.org/spotifyd.png)
+
+5. For more advanced client functionality, see the `/etc/spotifyd.conf` file and [consult the upstream documentation](https://docs.spotifyd.rs/configuration/index.html) on how to configure the daemon.
+
+## Reporting Bugs
+
+For issues directly relating to the installation or RPM packaging of spotifyd, please file bugs at [github.com/mbooth101/spotifyd-rpm/issues](https://github.com/mbooth101/spotifyd-rpm/issues).
+
+For defects in the spotifyd software itself, and only when you are 100% sure the problem is not a RPM packaging error, please file bugs at [github.com/Spotifyd/spotifyd/issues](https://github.com/Spotifyd/spotifyd/issues).
+
+## Source and Binary Locations
+
+This repository contains the source specfiles for creating the RPMs.
+
+The binary RPMs are built and distributed by a [COPR repository](https://copr.fedorainfracloud.org/coprs/mbooth/spotifyd/).
+
--- package_tree.sh
+++ package_tree.sh
@@ -1,39 +1,72 @@
 #!/bin/bash
 
+NAME=spotifyd
+REPO=copr:copr.fedorainfracloud.org:mbooth:$NAME
 
-REPO=copr:copr.fedorainfracloud.org:mbooth:spotifyd
+sudo dnf --assumeyes copr enable mbooth/$NAME
 
-sudo dnf --assumeyes copr enable mbooth/spotifyd
+function pkg_name {
+	echo -n "$1" | rev | cut -f1,2 -d- --complement | rev
+}
+
+# All RPMs in the repo
+ALL_RPMS=$(sudo dnf --repo=$REPO repoquery --available 2>/dev/null)
 
 # Pre-fetch SRPMs for all avilable RPMs
-declare -A SRPM_MAP
-for rpm in $(sudo dnf --repo=$REPO repoquery --available 2>/dev/null) ; do
-	rpm_name=$(echo -n "$rpm" | rev | cut -f1,2 -d- --complement | rev)
+declare -A SRPMNAME_VERREL_MAP
+declare -A RPM_SRPM_MAP
+for rpm in $ALL_RPMS ; do
+	rpm_name=$(pkg_name $rpm)
+	rpm_verrel=$(echo -n "$rpm" | rev | cut -f1,2 -d- | rev | sed -e 's/\(\.fc[0-9][0-9]\)\..*/\1/')
 	if [[ "$rpm" =~ .*\.src$ ]] ; then
+		srpm=$rpm
 		srpm_name=$rpm_name
+		srpm_verrel=$rpm_verrel
 	else
-		srpm=$(sudo dnf --repo=$REPO repoquery --sourcerpm $rpm_name 2>/dev/null | xargs)
-		srpm_name=$(echo -n "$srpm" | rev | cut -f1,2 -d- --complement | rev)
+		srpm=$(sudo dnf --repo=$REPO repoquery --sourcerpm ${rpm_name}-${rpm_verrel} 2>/dev/null | xargs)
+		srpm_name=$(pkg_name $srpm)
+		srpm_verrel=$(echo -n "$srpm" | rev | cut -f1,2 -d- | rev | sed -e 's/\(\.fc[0-9][0-9]\)\..*/\1/')
 	fi
-	SRPM_MAP["$rpm_name"]="$srpm_name"
+	srpm_highest_verrel=${SRPMNAME_VERREL_MAP[$srpm_name]}
+	if [ -z "$srpm_highest_verrel" ] ; then
+		SRPMNAME_VERREL_MAP[$srpm_name]=$srpm_verrel
+	else
+		rpmdev-vercmp $srpm_highest_verrel $srpm_verrel &>/dev/null
+		if [ "$?" = 12 ] ; then
+			SRPMNAME_VERREL_MAP[$srpm_name]=$srpm_verrel
+		fi
+	fi
+	RPM_SRPM_MAP[$rpm]=$srpm
 	echo -n "." 1>&2
 done
 echo 1>&2
 
 # Generate dependency tree
 echo "digraph deps {" > package_tree.dot
-for rpm in ${!SRPM_MAP[@]} ; do
-	deps=$(sudo dnf --repo=$REPO repoquery --available --recursive --whatrequires $rpm 2>/dev/null)
-	srpm=${SRPM_MAP[$rpm]}
+echo "rankdir=\"LR\";" >> package_tree.dot
+for rpm in "${!RPM_SRPM_MAP[@]}" ; do
+	srpm=${RPM_SRPM_MAP[$rpm]}
+	rpm_name=$(pkg_name $rpm)
+	rpm_verrel=$(echo -n "$rpm" | rev | cut -f1,2 -d- | rev | sed -e 's/\(\.fc[0-9][0-9]\)\..*/\1/')
+	srpm_name=$(pkg_name $srpm)
+	srpm_verrel=$(echo -n "$srpm" | rev | cut -f1,2 -d- | rev | sed -e 's/\(\.fc[0-9][0-9]\)\..*/\1/')
+	srpm_highest_verrel=${SRPMNAME_VERREL_MAP[$srpm_name]}
+	# Skip RPM if it belongs to a SRPM that is not the highest version
+	rpmdev-vercmp $srpm_highest_verrel $srpm_verrel &>/dev/null
+	if [ "$?" != 0 ] ; then
+		continue
+	fi
+	# Find RPMs that depend on this RPM
+	deps=$(sudo dnf --repo=$REPO repoquery --available --recursive --whatrequires ${rpm_name}-${rpm_verrel} 2>/dev/null)
 	for dep in $deps ; do
 		dep_name=$(echo -n "$dep" | rev | cut -f1,2 -d- --complement | rev)
 		if [[ "$dep" =~ .*\.src$ ]] ; then
-			dep_srpm=$dep_name
+			dep_srpm_name=$dep_name
 		else
-			dep_srpm=${SRPM_MAP[$dep_name]}
+			dep_srpm_name=$(pkg_name ${RPM_SRPM_MAP[$dep]})
 		fi
-		if [ "$srpm" != "$dep_srpm" ] ; then
-			echo "\"$dep_srpm\" -> \"$srpm\";"
+		if [ "$srpm_name" != "$dep_srpm_name" ] ; then
+			echo "\"$dep_srpm_name\" -> \"$srpm_name\";"
 		fi
 	done
 	echo -n "." 1>&2
@@ -41,5 +74,10 @@ done | sort | uniq >> package_tree.dot
 echo 1>&2
 echo "}" >> package_tree.dot
 dot package_tree.dot -Tpng -opackage_tree.png
-loupe package_tree.png
+
+if which loupe &>/dev/null ; then
+	loupe package_tree.png
+else
+	eog package_tree.png
+fi
 
